181fbac7c7f8ccb1591cbfeabacfd67b1aac79c6
chianyungcode <cnytechcode@gmail.com>
2025-05-23 07:11:48 +0700

    feat: Add gitsigns and obsidian-nvim plugins

--- chezmoi/dot_config/nvim/lua/plugins/obsidian-nvim.lua
+++ chezmoi/dot_config/nvim/lua/plugins/obsidian-nvim.lua
@@ -1,120 +1,40 @@
--- if true then
--- 	return {}
--- end
-
 return {
-	"epwalsh/obsidian.nvim",
+	"obsidian-nvim/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
 	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- event = {
+	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	--   -- refer to `:h file-pattern` for more examples
+	--   "BufReadPre path/to/my-vault/*.md",
+	--   "BufNewFile path/to/my-vault/*.md",
+	-- },
 	dependencies = {
 		-- Required.
 		"nvim-lua/plenary.nvim",
-	},
-	-- config = function(_, opts)
-	-- 	require("obsidian").setup(opts)
-	--
-	-- 	-- HACK: fix error, disable completion.nvim_cmp option, manually register sources
-	-- 	local cmp = require("cmp")
-	-- 	cmp.register_source("obsidian", require("cmp_obsidian").new())
-	-- 	cmp.register_source("obsidian_new", require("cmp_obsidian_new").new())
-	-- 	cmp.register_source("obsidian_tags", require("cmp_obsidian_tags").new())
-	-- end,
 
+		-- see above for full list of optional dependencies ☝️
+	},
+	---@module 'obsidian'
+	---@type obsidian.config.ClientOpts
 	opts = {
 		workspaces = {
 			{
 				name = "personal",
 				path = "/Users/chianyung/Desktop/vault-obsidian",
 			},
+			{
+				name = "work",
+				path = "~/vaults/work",
+			},
+		},
+		wiki_link_func = "use_alias_only",
+		completion = {
+			blink = true,
 		},
 
-		-- Completion settings
-		completion = {
-			nvim_cmp = true,
-		},
-		-- notes_subdir = "limbo", -- Subdirectory for notes
-		-- new_notes_location = "_encounter", -- Location for new notes
-		-- -- Settings for attachments
-		-- attachments = {
-		-- 	img_folder = "files", -- Folder for image attachments
-		-- },
-		--
-		-- -- Settings for daily notes
-		-- daily_notes = {
-		-- 	template = "note", -- Template for daily notes
-		-- },
-		--
-		-- -- Key mappings for Obsidian commands
-		-- mappings = {
-		-- 	-- "Obsidian follow"
-		-- 	["<leader>of"] = {
-		-- 		action = function()
-		-- 			return require("obsidian").util.gf_passthrough()
-		-- 		end,
-		-- 		opts = { noremap = false, expr = true, buffer = true },
-		-- 	},
-		-- 	-- Toggle check-boxes "obsidian done"
-		-- 	["<leader>od"] = {
-		-- 		action = function()
-		-- 			return require("obsidian").util.toggle_checkbox()
-		-- 		end,
-		-- 		opts = { buffer = true },
-		-- 	},
-		-- 	-- Create a new newsletter issue
-		-- 	["<leader>onn"] = {
-		-- 		action = function()
-		-- 			return require("obsidian").commands.new_note("Newsletter-Issue")
-		-- 		end,
-		-- 		opts = { buffer = true },
-		-- 	},
-		-- 	["<leader>ont"] = {
-		-- 		action = function()
-		-- 			return require("obsidian").util.insert_template("Newsletter-Issue")
-		-- 		end,
-		-- 		opts = { buffer = true },
-		-- 	},
-		-- },
-		--
-		-- -- Function to generate frontmatter for notes
-		-- note_frontmatter_func = function(note)
-		-- 	-- This is equivalent to the default frontmatter function.
-		-- 	local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-		--
-		-- 	-- `note.metadata` contains any manually added fields in the frontmatter.
-		-- 	-- So here we just make sure those fields are kept in the frontmatter.
-		-- 	if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-		-- 		for k, v in pairs(note.metadata) do
-		-- 			out[k] = v
-		-- 		end
-		-- 	end
-		-- 	return out
-		-- end,
-		--
-		-- -- Function to generate note IDs
-		-- note_id_func = function(title)
-		-- 	-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-		-- 	-- In this case a note with the title 'My new note' will be given an ID that looks
-		-- 	-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-		-- 	local suffix = ""
-		-- 	if title ~= nil then
-		-- 		-- If title is given, transform it into valid file name.
-		-- 		suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-		-- 	else
-		-- 		-- If title is nil, just add 4 random uppercase letters to the suffix.
-		-- 		for _ = 1, 4 do
-		-- 			suffix = suffix .. string.char(math.random(65, 90))
-		-- 		end
-		-- 	end
-		-- 	return tostring(os.time()) .. "-" .. suffix
-		-- end,
-		--
-		-- -- Settings for templates
-		-- templates = {
-		-- 	subdir = "templates", -- Subdirectory for templates
-		-- 	date_format = "%Y-%m-%d-%a", -- Date format for templates
-		-- 	gtime_format = "%H:%M", -- Time format for templates
-		-- 	tags = "", -- Default tags for templates
-		-- },
+		-- see below for full list of options 👇
 	},
 }
