a696f19623c64790ace1903a4927ce6ba5411bfa
chianyungcode <cnytechcode@gmail.com>
2025-05-25 19:08:14 +0700

    feat: Add configuration for folke/noice.nvim

---
+++ chezmoi/dot_config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,18 @@
+return
+-- lazy.nvim
+{
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+		views = {
+			-- This sets for cmdline position
+			cmdline_popup = {
+				position = {
+					row = "40%",
+					col = "50%",
+				},
+			},
+		},
+	},
+}
