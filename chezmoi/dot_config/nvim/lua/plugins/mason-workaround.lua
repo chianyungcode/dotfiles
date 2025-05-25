fbacb46912c65d7d6eba0a3b55124ffb2e7b116b
chianyungcode <cnytechcode@gmail.com>
2025-05-25 19:36:52 +0700

    chore: Remove unnecessary file after LazyVim update

--- chezmoi/dot_config/nvim/lua/plugins/mason-workaround.lua
+++ chezmoi/dot_config/nvim/lua/plugins/mason-workaround.lua
@@ -1,4 +1,5 @@
 -- https://www.reddit.com/r/neovim/comments/1kgu748/comment/mr41tkr/?share_id=s17nrPR2cp-IQzvHqwOH2&utm_content=2&utm_medium=android_app&utm_name=androidcss&utm_source=share&utm_term=1
+-- Need to delete this file after LazyVim has an update
 return {
 	{ "mason-org/mason.nvim", version = "1.11.0" },
 	{ "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
