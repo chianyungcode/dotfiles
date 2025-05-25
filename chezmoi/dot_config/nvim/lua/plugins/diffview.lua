4c0478e17165d6ed9f7468d5b2ddbdc5fd558b8f
chianyungcode <cnytechcode@gmail.com>
2025-05-25 19:15:02 +0700

    docs: Update comments for diffview plugin usage

--- chezmoi/dot_config/nvim/lua/plugins/diffview.lua
+++ chezmoi/dot_config/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,6 @@
+-- Plugin untuk melihat diff pada git
+-- Cara menggunakannya dengan masuk ke cmdline lalu ketik DiffviewOpen
+-- https://github.com/sindrets/diffview.nvim
 return {
 	"sindrets/diffview.nvim",
 }
