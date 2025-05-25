4779cbcfef29aa80244cff15413b1cdcea1e05d8
chianyungcode <cnytechcode@gmail.com>
2025-05-25 19:37:21 +0700

    style: Remove commented-out code for hover_select keymap

--- chezmoi/dot_config/nvim/lua/plugins/hover.lua
+++ chezmoi/dot_config/nvim/lua/plugins/hover.lua
@@ -27,7 +27,7 @@ return {
 
 		-- Setup keymaps
 		vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
-		vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+		-- vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
 		vim.keymap.set("n", "<C-p>", function()
 			require("hover").hover_switch("previous")
 		end, { desc = "hover.nvim (previous source)" })
