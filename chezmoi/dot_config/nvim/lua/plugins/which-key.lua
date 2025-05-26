73b44835d90960f361788517bcbc16ae49759d22
chianyungcode <cnytechcode@gmail.com>
2025-05-27 06:11:41 +0700

    feat: Add which-key configuration for VeryLazy event

---
+++ chezmoi/dot_config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	-- opts = {
+	-- 	spec = {
+	-- 		{
+	-- 			mode = { "n", "v" },
+	-- 			{ "<leader>e", group = "test-chian" },
+	-- 		},
+	-- 	},
+	-- },
+}
