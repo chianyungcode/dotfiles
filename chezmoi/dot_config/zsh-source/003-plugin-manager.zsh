1b0a60cb0ff7fcc31b75b1fda63f935412584595
chianyungcode <cnytechcode@gmail.com>
2025-06-02 16:56:28 +0700

    feat: Update zsh plugin manager configuration

---
+++ chezmoi/dot_config/zsh-source/003-plugin-manager.zsh
@@ -0,0 +1,54 @@
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+
+# === oh-my-zsh ===
+# plugins=(
+#     direnv
+#     git 
+#     git-commit 
+#     bun 
+#     1password 
+#     npm 
+#     you-should-use 
+#     zsh-autosuggestions 
+#     zsh-syntax-highlighting 
+#     aliases 
+#     copypath 
+#     brew 
+#     docker 
+#     docker-compose 
+#     gitignore 
+#     # zsh-abbr
+#     # asdf
+# )
+
+
+# Source Oh-My-Zsh
+# source $ZSH/oh-my-zsh.sh
+
+# === antidote ===
+# Ultra high performance install antidote
+# Set the root name of the plugins files (.txt and .zsh) antidote will use.
+zsh_plugins=${ZDOTDIR:-~}/.zsh_plugins
+
+# Ensure the .zsh_plugins.txt file exists so you can add plugins.
+[[ -f ${zsh_plugins}.txt ]] || touch ${zsh_plugins}.txt
+
+# Lazy-load antidote from its functions directory.
+fpath=(/path/to/antidote/functions $fpath)
+autoload -Uz antidote
+
+# Generate a new static file whenever .zsh_plugins.txt is updated.
+if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
+  antidote bundle <${zsh_plugins}.txt >|${zsh_plugins}.zsh
+fi
+
+# Source your static plugins file.
+source ${zsh_plugins}.zsh
+
+# Source or load antidote
+source /Users/chianyung/.antidote/antidote.zsh
+# Below if installing using homebrew
+# source $(brew --prefix)/opt/antidote/share/antidote/antidote.zsh
+
