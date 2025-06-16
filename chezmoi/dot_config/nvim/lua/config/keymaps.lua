d1416ba71ef73631ad80bb4f56ecdac22aedc1c7
chianyungcode <cnytechcode@gmail.com>
2025-06-16 14:18:25 +0700

    fix: Adjust search behavior for 'n' and 'N' keymaps

--- chezmoi/dot_config/nvim/lua/config/keymaps.lua
+++ chezmoi/dot_config/nvim/lua/config/keymaps.lua
@@ -7,5 +7,12 @@ vim.keymap.set("n", "<leader>wl", "<Plug>(WormholeLabels)", { desc = "Wormhole L
 vim.keymap.set("n", "<Esc>", "<Plug>(WormholeCloseLabels)", { desc = "Wormhole Close Labels" })
 -- vim.keymap.set("n", "<leader>e", function() end, { desc = "Chian" })
 
+-- Fix keymap behavior:
+-- When pressing '?' and 'n', the search should move to the top (previous match),
+-- not to the bottom (next match).
+-- This adjusts the search direction to be consistent with expected behavior.
+-- vim.keymap.set("n", "n", "n", { noremap = true })
+-- vim.keymap.set("n", "N", "N", { noremap = true })
+
 -- Syntax
 -- vim.keymap.set("mode-vim", "keymaps", "command / function", { desc = "Wormhole Close Labels" })
---
+++ chezmoi/dot_config/nvim/lua/plugins/flash-nvim.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {
+		search = {
+			multi_window = false,
+			forward = false,
+		},
+	},
+	-- stylua: ignore
+}
