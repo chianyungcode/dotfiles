af517435ef37a75519867342ba00a0832f195d56
chianyungcode <cnytechcode@gmail.com>
2025-06-28 11:42:25 +0700

    chore: update gitsigns.nvim plugin configuration

--- chezmoi/dot_config/nvim/lua/plugins/gitsigns.lua
+++ chezmoi/dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,11 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	-- config = function()
+	-- 	require("gitsigns").setup({
+	-- 		current_line_blame_opts = {
+	-- 			virt_text = true,
+	-- 			virt_text_priority = 100,
+	-- 		},
+	-- 	})
+	-- end,
 }
