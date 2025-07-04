1e11d35e4b2a90356241ef9299fbb1d37248baf8
chianyungcode <cnytechcode@gmail.com>
2025-07-04 19:37:38 +0700

    change diagnostic color and disabled the plugin

--- chezmoi/dot_config/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ chezmoi/dot_config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"rachartier/tiny-inline-diagnostic.nvim",
+		enabled = false,
 		event = "VeryLazy", -- Or `LspAttach`
 		priority = 1000, -- needs to be loaded in first
 		-- opts = {
@@ -9,16 +10,20 @@ return {
 		config = function()
 			vim.opt.updatetime = 100
 			vim.diagnostic.config({ virtual_text = false })
-			-- vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#f76464" })
-			-- vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = "#f7bf64" })
-			-- vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = "#64bcf7" })
-			-- vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = "#64f79d" })
-			require("chezmoi.dot_config.nvim.lua.plugins.tiny-inline-diagnostic").setup({
+			vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#f76464" })
+			vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = "#f7bf64" })
+			vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = "#64bcf7" })
+			vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = "#64f79d" })
+			require("tiny-inline-diagnostic").setup({
 				-- presets = "amongus",
 				options = {
 					multilines = true,
 					-- multiple_diag_under_cursor = true,
 					show_all_diags_on_cursorline = false,
+					virt_texts = {
+						-- Priority for virtual text display
+						priority = 3000,
+					},
 				},
 				blend = {
 					factor = 0.1,
