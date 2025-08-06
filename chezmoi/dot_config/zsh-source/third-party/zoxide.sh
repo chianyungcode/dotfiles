57ceeeb8d57886dce340f275b90844546c7b7843
chianyungcode <cnytechcode@gmail.com>
2025-08-06 16:26:21 +0700

    feat: add fzf_zoxide_widget

--- chezmoi/dot_config/zsh-source/third-party/zoxide.sh
+++ chezmoi/dot_config/zsh-source/third-party/zoxide.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # if [[ "$(command -v zoxide)" ]] && [[ -n ${ZSH_NAME} ]]; then
 #   # eval eval "$(zoxide init --cmd cd zsh)"
 #   eval "$(zoxide init zsh)"
@@ -17,3 +19,26 @@
 #     ll
 #   }
 # fi
+#
+
+# Widget Zsh: pilih direktori dari zoxide dengan preview dan langsung cd
+fzf_zoxide_widget() {
+  local selection dir
+  selection=$(zoxide query -ls | sort -nr | fzf \
+    --height=40% \
+    --reverse \
+    --prompt="Zoxide > " \
+    --preview 'exa -1 --color=always {2} 2>/dev/null || ls -la --color=always {2}' \
+    --preview-window=right:40%:wrap)
+
+  # Ambil kolom path saja (kolom kedua)
+  dir=$(echo "$selection" | awk '{ $1=""; sub(/^ /,""); print }')
+
+  if [[ -n $dir ]]; then
+    builtin cd "$dir" || return
+    zle accept-line
+  fi
+}
+
+zle -N fzf_zoxide_widget
+bindkey '^[^Z' fzf_zoxide_widget
