e98e35122a4adc437f87a5de3f8ade1ea87061f6
seraphynee <seraphyne31@gmail.com>
2025-08-07 14:56:16 +0700

    feat: add lsp and vivid in pacman packages

--- chezmoi/dot_config/nvim/lua/plugins/mason.lua
+++ chezmoi/dot_config/nvim/lua/plugins/mason.lua
@@ -1,45 +1,46 @@
 return {
-	{
-		"mason-org/mason-lspconfig.nvim", -- Ini yang memberitahu ke nvim-lspconfig mana LSP yang harus dijalankan
-		version = "1.32.0",
-		opts = {},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					-- Semua dibawah ini dinamakan registries
-					"templ", -- Go
-					"markdown_oxide",
-					"ts_ls", -- Kode error diagnostic 'ts'
-					"lua_ls",
-					"eslint", -- Kode error diagnostic 'typesript-eslint'
-					"biome",
-					"vtsls",
-					"tailwindcss",
-					"astro",
-					"gopls",
-					"html",
-					"jsonls",
-					"denols",
-					"nil_ls",
-					"taplo",
-				},
-				automatic_enable = {
-					exclude = {
-						"marksman",
-						"harper-ls",
-						"shellcheck",
-					},
-				},
-			})
-		end,
-	},
+  {
+    "mason-org/mason-lspconfig.nvim", -- Ini yang memberitahu ke nvim-lspconfig mana LSP yang harus dijalankan
+    version = "1.32.0",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          -- Semua dibawah ini dinamakan registries
+          "templ", -- Go
+          "markdown_oxide",
+          "ts_ls", -- Kode error diagnostic 'ts'
+          "lua_ls",
+          "eslint", -- Kode error diagnostic 'typesript-eslint'
+          "biome",
+          "vtsls",
+          "tailwindcss",
+          "astro",
+          "gopls",
+          "html",
+          "jsonls",
+          "denols",
+          "nil_ls",
+          "taplo",
+          "yamlls",
+        },
+        automatic_enable = {
+          exclude = {
+            "marksman",
+            "harper-ls",
+            "shellcheck",
+          },
+        },
+      })
+    end,
+  },
 
-	{
-		"mason-org/mason.nvim", -- Ini seperti UI nya di Neovim untuk menginstall berbagai LSP, formatters, linters dsb
-		version = "1.11.0",
-	},
+  {
+    "mason-org/mason.nvim", -- Ini seperti UI nya di Neovim untuk menginstall berbagai LSP, formatters, linters dsb
+    version = "1.11.0",
+  },
 }
