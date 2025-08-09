52c3403886b57cfc06ab52a07faa98dad1cea0af
chianyungcode <cnytechcode@gmail.com>
2025-08-09 07:50:41 +0700

    feat: disable tiny-inline-diagnostics because it's buggy

--- chezmoi/dot_config/nvim/lua/plugins/code-actions.lua
+++ chezmoi/dot_config/nvim/lua/plugins/code-actions.lua
@@ -42,38 +42,14 @@ return {
     end,
   },
   {
+    -- A bit buggy, sometimes diagnostic not showing
     "rachartier/tiny-inline-diagnostic.nvim",
-    enabled = true,
+    enabled = false,
     event = "VeryLazy", -- Or `LspAttach`
     priority = 1000, -- needs to be loaded in first
-    -- opts = {
-    -- 	presets = "ghost",
-    -- },
     config = function()
-      vim.opt.updatetime = 100
-      vim.diagnostic.config({ virtual_text = false })
-      vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#f76464" })
-      vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = "#f7bf64" })
-      vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = "#64bcf7" })
-      vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = "#64f79d" })
-      require("tiny-inline-diagnostic").setup({
-        -- presets = "amongus",
-        options = {
-          multilines = true,
-          -- multiple_diag_under_cursor = true,
-          show_all_diags_on_cursorline = false,
-          virt_texts = {
-            -- Priority for virtual text display
-            priority = 3000,
-          },
-        },
-        blend = {
-          factor = 0.1,
-        },
-        signs = {
-          diag = "ඞ", -- Ini di copy dari folder github nya manual di https://github.com/rachartier/tiny-inline-diagnostic.nvim/blob/main/lua/tiny-inline-diagnostic/presets.lua
-        },
-      })
+      require("tiny-inline-diagnostic").setup()
+      vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
     end,
   },
   {
