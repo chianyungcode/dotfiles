fde875e77909663195bf6f862030a8eea29a6a36
chianyungcode <cnytechcode@gmail.com>
2025-08-13 03:10:40 +0700

    feat: tidy up zsh and fish alias

--- chezmoi/dot_config/zsh-source/third-party/pet.sh
+++ chezmoi/dot_config/zsh-source/third-party/pet.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
+
 function pet-select() {
-	BUFFER=$(pet search --query "$LBUFFER")
-	CURSOR=$#BUFFER
-	zle redisplay
+  BUFFER=$(pet search --query "$LBUFFER")
+  CURSOR=$#BUFFER
+  zle redisplay
 }
 zle -N pet-select
 
 if [ -t 0 ]; then
-	stty -ixon
+  stty -ixon
 fi
 
 # Bind di emacs mode
@@ -15,3 +17,6 @@ bindkey '^f' pet-select
 bindkey -M viins '^f' pet-select
 # Bind di vi command mode
 bindkey -M vicmd '^f' pet-select
+
+alias pexec="pet exec -t"
+alias psc="pet search"
