50624c51258f32ae54c5b0a86edade36b15d0c8e
chianyungcode <cnytechcode@gmail.com>
2025-08-13 16:34:06 +0700

    feat: add tmux workspace with gum script shell

---
+++ chezmoi/bin/executable_gum-tmux-2.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e
+
+# Array path yang akan ditampilkan
+directories=(
+  "$HOME/.local/share/chezmoi"
+  "$HOME/Downloads"
+  "$HOME/Code/Personal/Projects/Backend/allweezy-backend"
+  "$HOME/Code/Personal/Projects/Frontend/allweezy"
+)
+
+tmux_session_names=(
+  "dotfiles"
+  "backend"
+  "frontend"
+)
+
+obsidian_dir="$HOME/Documents/vault-obsidian"
+
+# Loop array jadi input untuk gum filter
+PROJECT_DIR=$(for dir in "${directories[@]}"; do
+  echo "$dir"
+done | gum filter --placeholder "What is the path to directory")
+clear
+
+PUEUE_GROUP="tmux"
+clear
+
+SESSION_NAME=$(for sesion_name in "${tmux_session_names[@]}"; do
+  echo "$sesion_name"
+done | gum filter --placeholder "What is the tmux sesion name? ")
+clear
+
+# Exit jika salah satu variable kosong
+if [[ -z "$PROJECT_DIR" || -z "$PUEUE_GROUP" || -z "$SESSION_NAME" ]]; then
+  echo "❌ Error: All inputs (PROJECT_DIR, PUEUE_GROUP, SESSION_NAME) must be provided."
+  exit 1
+fi
+
+pueue add -g "$PUEUE_GROUP" "tmux new-session -d -s \"$SESSION_NAME\" -n IDE -c \"$PROJECT_DIR\""
+pueue add -g "$PUEUE_GROUP" sleep 5
+
+# WINDOW 1
+pueue add -g "$PUEUE_GROUP" "tmux send-keys -t \"$SESSION_NAME\":1.1 nvim C-m"
+pueue add -g "$PUEUE_GROUP" sleep 3
+
+# WINDOW 2
+pueue add -g "$PUEUE_GROUP" "tmux new-window -t \"$SESSION_NAME\":2 -n \"󰊢 \" -c \"$PROJECT_DIR\""
+pueue add -g "$PUEUE_GROUP" sleep 5
+
+pueue add -g "$PUEUE_GROUP" "tmux send-keys -t \"$SESSION_NAME\":2.1 lazygit C-m"
+pueue add -g "$PUEUE_GROUP" sleep 5
+
+# WINDOW 3
+pueue add -g "$PUEUE_GROUP" "tmux new-window -t \"$SESSION_NAME\":3 -n \"obsidian\" -c \"$obsidian_dir\""
+pueue add -g "$PUEUE_GROUP" sleep 5
+
+pueue add -g "$PUEUE_GROUP" "tmux send-keys -t \"$SESSION_NAME\":3.1 nvim C-m"
+pueue add -g "$PUEUE_GROUP" sleep 3
+
+# --
+
+pueue add -g "$PUEUE_GROUP" "tmux selectw -t \"$SESSION_NAME\":1"
+pueue add -g "$PUEUE_GROUP" sleep 2
+
+pueue add -g "$PUEUE_GROUP" "osascript -e 'display notification \"Success running all command with pueue cli\" with title \"gum-tmux.sh\"'"
