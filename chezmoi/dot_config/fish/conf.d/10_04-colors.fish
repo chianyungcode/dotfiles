ce364585488d0e73871f106827e6eda2ab6657ed
chianyungcode <cnytechcode@gmail.com>
2025-08-13 21:53:59 +0700

    feat: configure color for fish and ghostty

--- chezmoi/dot_config/fish/conf.d/10_04-colors.fish
+++ chezmoi/dot_config/fish/conf.d/10_04-colors.fish
@@ -1,21 +1,44 @@
 # references: https://fishshell.com/docs/current/interactive.html#variables-color
+# = THEME 1 =
+# set -U fish_color_autosuggestion grey --dim
+# set -U fish_color_command green
+# set -U fish_color_error red
+# set -U fish_color_param cyan
+# set -U fish_color_redirections yellow
+# set -U fish_color_terminators white
+# set -U fish_color_valid_path purple
+#
+# set fish_pager_color_background black
+# set fish_pager_color_prefix black
+# set fish_pager_color_completion black
+# set fish_pager_color_description black
+#
+# set fish_pager_color_selected_background white
+# set fish_pager_color_selected_prefix yellow
+# set fish_pager_color_selected_completion red
+# set fish_pager_color_selected_description yellow
+#
+# set fish_pager_color_background --background=EAE4D5
+# set fish_pager_color_secondary_background --background=B8CFCE
+
+# = THEME 2 =
+# references: https://fishshell.com/docs/current/interactive.html#variables-color
 set -U fish_color_autosuggestion grey --dim
 set -U fish_color_command green
 set -U fish_color_error red
 set -U fish_color_param cyan
 set -U fish_color_redirections yellow
 set -U fish_color_terminators white
-set -U fish_color_valid_path purple
+set -U fish_color_valid_path '#7CA6E9'
 
-set fish_pager_color_background black
-set fish_pager_color_prefix black
-set fish_pager_color_completion black
-set fish_pager_color_description black
+set -U fish_pager_color_progress '#fff' --background='#2B7388'
 
-set fish_pager_color_selected_background white
-set fish_pager_color_selected_prefix yellow
-set fish_pager_color_selected_completion red
-set fish_pager_color_selected_description yellow
+set -U fish_pager_color_background
+set -U fish_pager_color_prefix white
+set -U fish_pager_color_completion white
+set -U fish_pager_color_description yellow
 
-set fish_pager_color_background --background=EAE4D5
-set fish_pager_color_secondary_background --background=B8CFCE
+set -U fish_pager_color_selected_background --background=grey
+set -U fish_pager_color_selected_prefix '#000000'
+set -U fish_pager_color_selected_completion '#000000'
+set -U fish_pager_color_selected_description '#000000'
