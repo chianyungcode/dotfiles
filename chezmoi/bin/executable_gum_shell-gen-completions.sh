b2460c856602d0bd2282566446c998d80871d6d9
seraphynee <seraphyne31@gmail.com>
2025-08-15 06:13:02 +0700

    feat: change path zsh completions

--- chezmoi/bin/executable_gum_shell-gen-completions.sh
+++ chezmoi/bin/executable_gum_shell-gen-completions.sh
@@ -3,7 +3,7 @@
 base_cmd=$(gum input --placeholder "what is the cli command to generate completion?")
 filename=$(gum input --placeholder "what is the cli name?")
 
-dest_zsh="$HOME/.local/share/chezmoi/chezmoi/dot_config/zsh-source/completions/_$filename"
+dest_zsh="$HOME/.local/share/chezmoi/chezmoi/dot_config/zsh/completions/_$filename"
 dest_fish="$HOME/.local/share/chezmoi/chezmoi/dot_config/fish/completions/$filename.fish"
 
 eval "$base_cmd zsh" >"$dest_zsh"
