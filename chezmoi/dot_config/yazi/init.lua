135c4d0d736ae45c13bc7445261966910159ee1c
chianyungcode <cnytechcode@gmail.com>
2025-08-26 18:29:02 +0700

    feat: add jump-to-char and relative-motions plugins for yazi

--- chezmoi/dot_config/yazi/init.lua
+++ chezmoi/dot_config/yazi/init.lua
@@ -2,3 +2,4 @@ require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 })
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
