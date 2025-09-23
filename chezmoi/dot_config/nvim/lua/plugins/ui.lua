2608fee8728f14b7026a9d894ec8a5ad5f70b110
chianyungcode <cnytechcode@gmail.com>
2025-09-23 16:37:28 +0700

    feat(ui): enable dropbar.nvim with keymaps and disable bufferline.nvim

--- chezmoi/dot_config/nvim/lua/plugins/ui.lua
+++ chezmoi/dot_config/nvim/lua/plugins/ui.lua
@@ -3,13 +3,19 @@ return {
     -- Breadcrumb menunjukkan file path diatas
     -- https://github.com/Bekaboo/dropbar.nvim
     "Bekaboo/dropbar.nvim",
-    enabled = false,
+    enabled = true,
+    config = function()
+      local dropbar_api = require("dropbar.api")
+      vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+      vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+      vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+    end,
     -- Changed this
   },
   {
     -- Buffer tab line on top
     "akinsho/bufferline.nvim",
-    enabled = true,
+    enabled = false,
     keys = {
       {
         "<A-1>",
