b5aa090a182a00c0ac6f0a7c2a815604f7587643
chianyungcode <cnytechcode@gmail.com>
2025-10-20 21:25:21 +0700

    chore(nvim): add dropbar.nvim setup with folder icons and fix leader keymap case

--- chezmoi/dot_config/nvim/lua/plugins/ui.lua
+++ chezmoi/dot_config/nvim/lua/plugins/ui.lua
@@ -5,12 +5,21 @@ return {
     "Bekaboo/dropbar.nvim",
     enabled = true,
     config = function()
+      require("dropbar").setup({
+        icons = {
+          kinds = {
+            symbols = {
+              Folder = " ",
+            },
+          },
+        },
+      })
+
       local dropbar_api = require("dropbar.api")
-      vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+      vim.keymap.set("n", "<leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
       vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
       vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
     end,
-    -- Changed this
   },
   {
     -- Buffer tab line on top
