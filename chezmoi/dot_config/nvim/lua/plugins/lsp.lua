f3193367f772b216e5170d4096a8f84d3807e523
seraphynee <seraphyne31@gmail.com>
2025-10-26 14:55:48 +0700

    fix: change deprecated lazyvim.plugin.lsp.keymaps method

--- chezmoi/dot_config/nvim/lua/plugins/lsp.lua
+++ chezmoi/dot_config/nvim/lua/plugins/lsp.lua
@@ -57,12 +57,16 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- disable a keymap
-      -- Ini adalah default untuk hover kode, keymap ini saya disable karena berbenturan dengan plugin hover.lua . Sebenarnya sama saja fungsinya sama-sama untuk menghover suatu kode, tetapi dengan plugin bisa di customize lebih lagi.
-      keys[#keys + 1] = { "K", false }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            -- Disable default hover keymap to avoid conflict with hover.lua
+            { "K", false },
+          },
+        },
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
