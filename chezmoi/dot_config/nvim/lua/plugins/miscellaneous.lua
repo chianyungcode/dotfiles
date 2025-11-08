37cd21277d79e31474e60be059fdfa47364b996a
chianyungcode <cnytechcode@gmail.com>
2025-11-08 12:08:10 +0700

    refactor(nvim): move substitute, yanky, timber to text-editing.lua and disable tiny-glimmer, lazyclip

--- chezmoi/dot_config/nvim/lua/plugins/miscellaneous.lua
+++ chezmoi/dot_config/nvim/lua/plugins/miscellaneous.lua
@@ -1,41 +1,4 @@
 return {
-  {
-    "gbprod/substitute.nvim",
-    enabled = false,
-    config = function()
-      require("substitute").setup({})
-
-      -- masih bentrok dengan flash.nvim masih bingung untuk keymapping yang sesuai nantinya
-      -- Lua
-      vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
-      vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
-      vim.keymap.set("n", "Z", require("substitute").eol, { noremap = true }) -- end of line
-      vim.keymap.set("x", "z", require("substitute").visual, { noremap = true })
-
-      vim.keymap.set("n", "sx", require("substitute.exchange").operator, { noremap = true })
-      vim.keymap.set("n", "sxx", require("substitute.exchange").line, { noremap = true })
-      vim.keymap.set("x", "X", require("substitute.exchange").visual, { noremap = true })
-      vim.keymap.set("n", "sxc", require("substitute.exchange").cancel, { noremap = true })
-    end,
-  },
-
-  -- https://github.com/gbprod/yanky.nvim
-  -- DESC: add more capabilities to yank
-  {
-    "gbprod/yanky.nvim",
-    enabled = false,
-    config = function()
-      require("yanky").setup({})
-
-      vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
-      vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
-      vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
-      vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
-
-      vim.keymap.set("n", "<C-p>", "<Plug>(YankyPreviousEntry)") -- ctrl + shift + p
-      vim.keymap.set("n", "<C-n>", "<Plug>(YankyNextEntry)") -- ctrl + shift + n
-    end,
-  },
 
   -- https://github.com/mluders/comfy-line-numbers.nvim
   -- DESC: change line numbers to more left-handed, example instead of 6j it will be 11j
@@ -262,20 +225,6 @@ return {
     cmd = "ShowkeysToggle",
   },
 
-  -- https://github.com/Goose97/timber.nvim
-  -- DESC: Neovim plugin to quickly insert log statements and capture log output
-  {
-    "Goose97/timber.nvim",
-    enabled = false,
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("timber").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
-  },
-
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -353,13 +302,20 @@ return {
 
   {
     "rachartier/tiny-glimmer.nvim",
-    enabled = true,
+    enabled = false,
     -- dependencies = { "gbprod/yanky.nvim" },
     event = "VeryLazy",
     priority = 10, -- Low priority to catch other plugins' keybindings
+    keys = {
+      "n",
+      "N",
+    },
     config = function()
       require("tiny-glimmer").setup({
         overwrite = {
+          search = {
+            enabled = true,
+          },
           undo = {
             enabled = true,
           },
