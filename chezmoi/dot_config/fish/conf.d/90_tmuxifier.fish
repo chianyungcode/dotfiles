78ce0fd8e8f684893df32ece0c7c8dfc72445473
chianyungcode <cnytechcode@gmail.com>
2025-11-08 17:55:01 +0700

    perf(dotfiles): skip tmuxifier init eval in fish and zsh configs to reduce startup time

--- chezmoi/dot_config/fish/conf.d/90_tmuxifier.fish
+++ chezmoi/dot_config/fish/conf.d/90_tmuxifier.fish
@@ -1,1 +1,2 @@
-eval (tmuxifier init - fish) # tmuxifier completions
+# Skip `eval (tmuxifier init - fish)` to avoid the extra ~20–30 ms startup cost; copy tmuxifier's completion scripts into Fish's `completions/` directory so they load lazily instead.
+# eval (tmuxifier init - fish)  # original tmuxifier completions
--- chezmoi/dot_config/zsh/conf.d/third-party/tmuxifier.sh
+++ chezmoi/dot_config/zsh/conf.d/third-party/tmuxifier.sh
@@ -1,1 +1,2 @@
-eval "$(tmuxifier init -)" # tmuxifier completions
+# # Skip `eval (tmuxifier init -)` to avoid the extra ~20–30 ms startup cost; copy tmuxifier's completion scripts into Zsh's `completions/` directory so they load lazily instead.
+# eval "$(tmuxifier init -)" # tmuxifier completions
