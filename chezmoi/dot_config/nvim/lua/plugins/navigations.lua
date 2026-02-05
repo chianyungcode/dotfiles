695c5aae45a9cb83fb583cc9e004930402150674
chianyungcode <cnytechcode@gmail.com>
2026-02-05 16:27:00 +0700

    feat(nvim): add smart-motion nvim plugin

--- chezmoi/dot_config/nvim/lua/plugins/navigations.lua
+++ chezmoi/dot_config/nvim/lua/plugins/navigations.lua
@@ -237,6 +237,29 @@ return {
     end,
   },
 
+  -- https://github.com/FluxxField/smart-motion.nvim
+  -- DESC: alternative to flash, leap, hop nvim
+  {
+    "FluxxField/smart-motion.nvim",
+    opts = {
+      presets = {
+        words = true, -- w, b, e, ge
+        lines = true, -- j, k
+        search = true, -- s, f, F, t, T, ;, ,, gs
+        delete = true, -- d, dt, dT, rdw, rdl
+        yank = true, -- y, yt, yT, ryw, ryl
+        change = true, -- c, ct, cT
+        paste = true, -- p, P
+        treesitter = true, -- ]], [[, ]c, [c, ]b, [b, daa, caa, yaa, dfn, cfn, yfn, saa
+        diagnostics = true, -- ]d, [d, ]e, [e
+        git = true, -- ]g, [g
+        quickfix = true, -- ]q, [q, ]l, [l
+        marks = true, -- g', gm
+        misc = true, -- . (repeat), gmd, gmy
+      },
+    },
+  },
+
   {
     -- youtube videos: https://www.youtube.com/watch?v=p_sVgHS2zcA
     "folke/flash.nvim",
