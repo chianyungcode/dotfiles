974c56bdd3ccdfd504f56776548827eb56e76892
chianyungcode <cnytechcode@gmail.com>
2026-02-14 22:29:12 +0700

    feat: add keybind to sidekick plugin nvim

--- chezmoi/dot_config/nvim/lua/plugins/ai.lua
+++ chezmoi/dot_config/nvim/lua/plugins/ai.lua
@@ -75,6 +75,36 @@ return {
         mode = { "n", "v" },
       },
       {
+        "<leader>ad",
+        function()
+          require("sidekick.cli").close()
+        end,
+        desc = "Detach a CLI Session",
+      },
+      {
+        "<leader>at",
+        function()
+          require("sidekick.cli").send({ msg = "{this}" })
+        end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>af",
+        function()
+          require("sidekick.cli").send({ msg = "{file}" })
+        end,
+        desc = "Send File",
+      },
+      {
+        "<leader>av",
+        function()
+          require("sidekick.cli").send({ msg = "{selection}" })
+        end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
         "<leader>ap",
         function()
           require("sidekick.cli").prompt()
