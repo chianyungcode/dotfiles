026fb6aeac2645a6f791b6fafb0752381189a091
chianyungcode <cnytechcode@gmail.com>
2026-02-20 23:34:49 +0700

    fix: configure atuin arrow up & ctrl-r in insert mode & vi-mode

--- chezmoi/dot_config/fish/conf.d/90_atuin.fish
+++ chezmoi/dot_config/fish/conf.d/90_atuin.fish
@@ -1,4 +1,19 @@
 set -gx ATUIN_NOBIND true
 atuin init fish | source
 
-bind up _atuin_bind_up # https://docs.atuin.sh/configuration/key-binding/#fish
+# In vi mode, Up in insert mode needs an explicit binding too.
+if string match -q '4.*' $version
+    bind up _atuin_bind_up # https://docs.atuin.sh/configuration/key-binding/#fish
+    if bind -M insert >/dev/null 2>&1
+        bind -M insert up _atuin_bind_up
+    end
+else
+    bind -k up _atuin_bind_up
+    bind \eOA _atuin_bind_up
+    bind \e\[A _atuin_bind_up
+    if bind -M insert >/dev/null 2>&1
+        bind -M insert -k up _atuin_bind_up
+        bind -M insert \eOA _atuin_bind_up
+        bind -M insert \e\[A _atuin_bind_up
+    end
+end
