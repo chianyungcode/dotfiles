e0c39c68942ad648a7b162b71ac56bb489b06faa
seraphynee <seraphyne31@gmail.com>
2026-03-16 16:34:39 +0700

    feat: add sesh-connect-picker script to the PATH and use it when using tmux

--- chezmoi/dot_config/fish/conf.d/90_sesh.fish
+++ chezmoi/dot_config/fish/conf.d/90_sesh.fish
@@ -1,22 +1,6 @@
-function sesh-sessions
-    # Pilih session
-    set session (sesh list -t | sed 's/^/󱂬 /' | fzf \
-        --height 40% \
-        --border-label ' sesh ' \
-        --border \
-        --prompt '⚡  ' \
-        --preview 'tmux capture-pane -pe -t {2..}')
-
-    # Jika tidak ada session, keluar
-    test -z "$session"; and return
-
-    # Connect ke session yang dipilih
-    sesh connect (string replace -r '^󱂬 ' '' "$session")
-end
-
 # Untuk default mode (emacs mode)
-bind \eu sesh-sessions
+bind \eu sesh-connect-picker
 
 # Untuk vi mode jika diaktifkan
-bind -M insert \eu sesh-sessions
-bind -M default \eu sesh-sessions
+bind -M insert \eu sesh-connect-picker
+bind -M default \eu sesh-connect-picker
