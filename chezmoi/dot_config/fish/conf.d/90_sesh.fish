255287cd7b1cf45e4143208ae97ada44545a5f3d
chianyungcode <cnytechcode@gmail.com>
2026-03-17 22:43:47 +0700

    fix: keybinding for sesh tmux session in fish and tmux

--- chezmoi/dot_config/fish/conf.d/90_sesh.fish
+++ chezmoi/dot_config/fish/conf.d/90_sesh.fish
@@ -1,6 +1,13 @@
+function __sesh_connect_picker_widget
+    # Jalankan lewat path eksekusi normal fish agar TUI seperti gum bisa
+    # mengambil alih terminal dengan benar.
+    commandline --replace 'sesh-connect-picker.sh'
+    commandline -f execute
+end
+
 # Untuk default mode (emacs mode)
-bind \eu sesh-connect-picker
+bind \eu __sesh_connect_picker_widget
 
 # Untuk vi mode jika diaktifkan
-bind -M insert \eu sesh-connect-picker
-bind -M default \eu sesh-connect-picker
+bind -M insert \eu __sesh_connect_picker_widget
+bind -M default \eu __sesh_connect_picker_widget
