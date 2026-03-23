73d0810301e60e0bee8833479f5e9a6357b22c57
chianyungcode <cnytechcode@gmail.com>
2026-03-23 16:31:22 +0700

    feat(shell): add drift configuration to zsh and fish

---
+++ chezmoi/dot_config/fish/conf.d/90_drift.fish
@@ -0,0 +1,3 @@
+# add to ~/.config/fish/conf.d/drift.fish
+# set -x DRIFT_TIMEOUT 10 # seconds of inactivity (default: 120)
+# drift shell-init fish | source
---
+++ chezmoi/dot_config/zsh/conf.d/third-party/drift.sh
@@ -0,0 +1,3 @@
+# add to ~/.zshrc
+# export DRIFT_TIMEOUT=120 # seconds of inactivity (default: 120)
+# eval "$(drift shell-init zsh)"
