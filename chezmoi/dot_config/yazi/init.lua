86f87ab527c990bed9181957dd2299a7b9a4087a
chianyungcode <cnytechcode@gmail.com>
2026-05-16 17:14:37 +0700

    feat(yazi): configure new config for yazi

--- chezmoi/dot_config/yazi/init.lua
+++ chezmoi/dot_config/yazi/init.lua
@@ -1,7 +1,2 @@
-require("full-border"):setup({
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
-})
+-- require("full-border"):setup()
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
-require("starship"):setup()
-require("git"):setup()
