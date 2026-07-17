d45a7db0e48d9bb2ba49eef6be684a19c5bca7f8
chianyungcode <cnytechcode@gmail.com>
2026-07-17 19:38:59 +0700

    feat(jj): add workspace aliases

--- chezmoi/dot_config/fish/conf.d/90_jujutsu.fish
+++ chezmoi/dot_config/fish/conf.d/90_jujutsu.fish
@@ -1,1 +1,25 @@
 # COMPLETE=fish jj | source
+
+# Jujutsu aliases run in a child process, so they cannot change the parent
+# shell's directory themselves. Apply the paths printed by wq/wcd/wacd here.
+function jj --description 'Jujutsu wrapper with workspace navigation'
+    set -l subcommand ''
+    if test (count $argv) -gt 0
+        set subcommand $argv[1]
+    end
+
+    switch "$subcommand"
+        case wq wcd wacd
+            set -l destination (command jj $argv)
+            or return
+
+            if not test -d "$destination"
+                echo "jj: workspace directory does not exist: $destination" >&2
+                return 1
+            end
+
+            builtin cd -- "$destination"
+        case '*'
+            command jj $argv
+    end
+end
--- chezmoi/dot_config/zsh/conf.d/third-party/jujutsu.sh
+++ chezmoi/dot_config/zsh/conf.d/third-party/jujutsu.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env zsh
 
 # source <(COMPLETE=zsh jj)
+
+# Jujutsu aliases run in a child process, so they cannot change the parent
+# shell's directory themselves. Apply the paths printed by wq/wcd/wacd here.
+jj() {
+  case "${1-}" in
+    wq|wcd|wacd)
+      local destination
+      destination="$(command jj "$@")" || return
+
+      if [[ ! -d "$destination" ]]; then
+        echo "jj: workspace directory does not exist: $destination" >&2
+        return 1
+      fi
+
+      builtin cd -- "$destination"
+      ;;
+    *)
+      command jj "$@"
+      ;;
+  esac
+}
