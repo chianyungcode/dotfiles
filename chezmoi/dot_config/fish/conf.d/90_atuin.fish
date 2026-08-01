ef7c35e68bab14f17549c50d8e6530f2b807ef24
chianyungcode <cnytechcode@gmail.com>
2026-08-01 18:08:06 +0700

    fix(fish): atuin global search keybind

--- chezmoi/dot_config/fish/conf.d/90_atuin.fish
+++ chezmoi/dot_config/fish/conf.d/90_atuin.fish
@@ -3,15 +3,19 @@ atuin init fish | source
 
 # In vi mode, Up in insert mode needs an explicit binding too.
 if string match -q '4.*' $version
+    bind ctrl-r _atuin_search
     bind up _atuin_bind_up # https://docs.atuin.sh/configuration/key-binding/#fish
     if bind -M insert >/dev/null 2>&1
+        bind -M insert ctrl-r _atuin_search
         bind -M insert up _atuin_bind_up
     end
 else
+    bind \cr _atuin_search
     bind -k up _atuin_bind_up
     bind \eOA _atuin_bind_up
     bind \e\[A _atuin_bind_up
     if bind -M insert >/dev/null 2>&1
+        bind -M insert \cr _atuin_search
         bind -M insert -k up _atuin_bind_up
         bind -M insert \eOA _atuin_bind_up
         bind -M insert \e\[A _atuin_bind_up
