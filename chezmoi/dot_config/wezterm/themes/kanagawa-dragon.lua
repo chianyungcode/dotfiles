d875d6ea7de91df3c38936d4b181eeb3018577bd
chianyungcode <cnytechcode@gmail.com>
2026-08-02 16:43:26 +0700

    feat: add kanagawa dragon cli themes

---
+++ chezmoi/dot_config/wezterm/themes/kanagawa-dragon.lua
@@ -0,0 +1,17 @@
+return {
+	foreground = "#DCD7BA",
+	background = "#1F1F28",
+	cursor_bg = "#7AA89F",
+	cursor_border = "#7AA89F",
+	cursor_fg = "#1F1F28",
+	selection_bg = "#2D4F67",
+	selection_fg = "#DCD7BA",
+	ansi = {
+		"#1F1F28", "#C34043", "#98BB6C", "#E6C384",
+		"#658594", "#957FB8", "#6A9589", "#DCD7BA",
+	},
+	brights = {
+		"#54546D", "#E82424", "#7AA89F", "#FF9E3B",
+		"#7E9CD8", "#9CABCA", "#7AA89F", "#C8C093",
+	},
+}
