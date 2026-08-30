046f17b5aa2255bf4e310a28ed3b650d0a06096f
chianyungcode <cnytechcode@gmail.com>
2026-08-30 20:40:42 +0700

    feat(shell): add herdr abbreviations

--- chezmoi/dot_config/zsh/conf.d/third-party/herdr.sh
+++ chezmoi/dot_config/zsh/conf.d/third-party/herdr.sh
@@ -1,3 +1,14 @@
 for _f in ${HOME}/.config/herdr/plugins/github/herdr-automatic-rename-*/shell/hook.zsh(N); do
   source $_f; break
 done
+
+# Stop and delete a Herdr session in one command.
+function hsd() {
+  if [[ $# -ne 1 ]]; then
+    print -u2 "Usage: hsd <session>"
+    return 2
+  fi
+
+  local session="$1"
+  herdr session stop "$session" && herdr session delete "$session"
+}
